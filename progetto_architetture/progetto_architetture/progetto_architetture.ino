7c0dd50e98922521b33bf79ea8d84c18e6f617dc
nunziogiuliocaggegi <nunziogiuliocaggegi@gmail.com>
2015-05-25 18:50:12 +0200

    start

---
+++ progetto_architetture/progetto_architetture/progetto_architetture.ino
@@ -0,0 +1,37 @@
+    #include <SoftwareSerial.h>
+ 
+ 
+    
+    
+    const int btBoud = 115200;
+    const int pinBtTx = 11;
+    const int pinBtRx = 10;
+    const int pinLed = 13;
+    
+    SofwareSerial bt(pinBtTx,pinBtRx);
+    
+    
+    void setup()  
+    {  
+         
+     Serial.begin(9600);
+     while(!Serial);
+     
+     bt.begin(9600);
+     while(!bt);
+      
+     pinMode(pinLed,OUTPUT);
+    
+    }  
+      
+    void loop() 
+    {  
+     if(bt.available()){
+       char c = (char)bt.read();
+       if(c=='H')
+         digitalWrite(pinLed,HIGH);
+       if(c=='L')
+         digitalWrite(pinLed,HIGH);
+     } 
+       
+    }  
